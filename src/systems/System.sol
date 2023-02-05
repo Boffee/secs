b3a9924b6b39dde5e0840006b54ab0d88139aa64
Boffee <zhenbangli@gmail.com>
2023-02-05 01:11:01 -0600

    refactor: use openzeppelin Context

--- src/systems/System.sol
+++ src/systems/System.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0;
 
+import "@openzeppelin/contracts/utils/Context.sol";
 import "solecs/interfaces/ISystem.sol";
 import "solecs/interfaces/IUint256Component.sol";
 import "solecs/interfaces/IWorld.sol";
 import "solecs/Ownable.sol";
-import "../utils/Context.sol";
 
 abstract contract System is ISystem, Ownable, Context {
     IUint256Component public immutable COMPONENTS;
