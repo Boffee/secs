c197f40832ecf483e52718384cc6c14ffc5c4c82
Boffee <zhenbangli@gmail.com>
2023-02-06 20:28:20 -0600

    test: deploy systems

--- test/DeployLib.t.sol
+++ test/DeployLib.t.sol
@@ -4,10 +4,12 @@ pragma solidity ^0.8.17;
 import "forge-std/Test.sol";
 import "forge-std/console2.sol";
 import "secs/libraries/ComponentGetter.sol";
+import "secs/libraries/SystemGetter.sol";
 import "./DeployLib.sol";
 
 contract DeployLibTest is Test {
     using ComponentGetter for IUint256Component;
+    using SystemGetter for IUint256Component;
 
     IWorld world;
 
@@ -36,10 +38,29 @@ contract DeployLibTest is Test {
         assertEq(component.world(), address(world));
 
         component = components.ownerComponent();
-        console2.logBytes(components.ownerComponent().getRawValue(123));
         assertEq(component.world(), address(world));
 
         component = components.symbolComponent();
         assertEq(component.world(), address(world));
+    }
+
+    function testSystems() public {
+        IUint256Component systems = world.systems();
+        System system;
+
+        system = systems.approveSystem();
+        assertEq(address(system.WORLD()), address(world));
+
+        system = systems.burnSystem();
+        assertEq(address(system.WORLD()), address(world));
+
+        system = systems.safeTransferFromSystem();
+        assertEq(address(system.WORLD()), address(world));
+
+        system = systems.setApprovalForAllSystem();
+        assertEq(address(system.WORLD()), address(world));
+
+        system = systems.transferFromSystem();
+        assertEq(address(system.WORLD()), address(world));
     }
 }
