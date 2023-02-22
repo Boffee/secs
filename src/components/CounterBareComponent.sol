8ab8cf37d07c7b275f036f1b8de790fb37b72224
Boffee <zhenbangli@gmail.com>
2023-02-21 23:42:07 -0600

    feat: return new value after increment/decrement

--- src/components/CounterBareComponent.sol
+++ src/components/CounterBareComponent.sol
@@ -6,11 +6,19 @@ import "./Uint256BareComponent.sol";
 contract CounterBareComponent is Uint256BareComponent {
     constructor(address world, uint256 id) Uint256BareComponent(world, id) {}
 
-    function increment(uint256 entity, uint256 amount) public {
-        set(entity, getValue(entity) + amount);
+    function increment(uint256 entity, uint256 amount)
+        public
+        returns (uint256 newValue)
+    {
+        newValue = getValue(entity) + amount;
+        set(entity, newValue);
     }
 
-    function decrement(uint256 entity, uint256 amount) public {
-        set(entity, getValue(entity) - amount);
+    function decrement(uint256 entity, uint256 amount)
+        public
+        returns (uint256 newValue)
+    {
+        newValue = getValue(entity) - amount;
+        set(entity, newValue);
     }
 }
